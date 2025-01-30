93088392dc99c593812cd5a36e9bb867903653f5
nurmohammadpro <nur_mohammad1@outlook.com>
2025-01-31 00:55:02 +0600

    navbar and hero section complete

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "clr-primary": "#FFF9F4",
+        "clr-accent": "#FFA943",
+      },
+      fontFamily: {
+        sans: ['"Sora"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
